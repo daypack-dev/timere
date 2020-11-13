36183bb054d541cde72113fea3121e1241d613e3
Darren Ldl <darrenldldev@gmail.com>
2020-11-13 17:16:44 +1100

    Progress on parser

--- src/parser.ml
+++ src/parser.ml
@@ -1,4 +1,7 @@
-type typ =
+open MParser
+open Parser_components
+
+type ambiguous =
   | Years of int list
   | Months of Time.month list
   | Month_days of int list
@@ -7,7 +10,125 @@ type typ =
   | Minutes of int list
   | Seconds of int list
 
+type guess =
+  | Uncertain of ambiguous list
+  | Dot
+  | Comma
+  | To
+  | From
+
 type token = {
-  pos : int * int;
-  may_be : typ;
+  pos : int * int * int;
+  guess : guess;
 }
+
+type binary_op =
+  | Union
+  | Inter
+
+type ast =
+  | Tokens of token list
+  | Binary_op of binary_op * ast * ast
+  | Round_robin_pick of ast list
+
+type static_str_p = (string, unit) MParser.t
+
+let not_str : static_str_p = string "not"
+
+let next_slot_str : static_str_p = string "next-slot"
+
+let next_point_str : static_str_p =
+  attempt (string "next-point") <|> string "next-pt"
+
+let next_batch_str : static_str_p = string "next-batch"
+
+let next_str : static_str_p = string "next"
+
+let point_str : static_str_p = string "point"
+
+let slot_str : static_str_p = string "slot"
+
+let points_str : static_str_p = string "points"
+
+let slots_str : static_str_p = string "slots"
+
+let batch_str : static_str_p = string "batch"
+
+let batches_str : static_str_p = string "batches"
+
+let of_str : static_str_p = string "of"
+
+let from_str : static_str_p = string "from"
+
+let to_str : static_str_p = string "to"
+
+let first_str : static_str_p = string "first"
+
+let last_str : static_str_p = string "last"
+
+let token_p : (token, unit) MParser.t =
+  get_pos
+  >>= fun pos ->
+  choice
+    [
+      attempt (char '.') >>$ Dot;
+      attempt (char ',') >>$ Comma;
+      attempt to_str >>$ To;
+      attempt from_str >>$ From;
+    ]
+  |>> fun guess -> { pos; guess }
+
+let tokens_p = many1 token_p
+
+let inter : (ast -> ast -> ast, unit) t =
+  string "&&" >> return (fun a b -> Binary_op (Inter, a, b))
+
+let union : (ast -> ast -> ast, unit) t =
+  string "||" >> return (fun a b -> Binary_op (Union, a, b))
+
+let round_robin_pick : (ast -> ast -> ast, unit) t =
+  string ">>" >> return (fun a b -> Round_robin_pick [ a; b ])
+
+let flatten_round_robin_select (e : ast) : ast =
+  let rec aux e =
+    match e with
+    | Tokens _ -> e
+    | Binary_op (op, e1, e2) -> Binary_op (op, aux e1, aux e2)
+    | Round_robin_pick l ->
+      l
+      |> List.to_seq
+      |> Seq.map aux
+      |> Seq.flat_map (fun e ->
+          match e with
+          | Round_robin_pick l -> List.to_seq l
+          | _ -> Seq.return e)
+      |> List.of_seq
+      |> fun l -> Round_robin_pick l
+  in
+  aux e
+
+let expr =
+  let rec expr mparser_state =
+    let inter_part =
+      attempt (char '(')
+      >> (spaces >> expr << spaces << char ')')
+         <|> (tokens_p |>> fun l -> Tokens l)
+    in
+    let ordered_select_part = chain_left1 inter_part round_robin_pick in
+    let union_part = chain_left1 ordered_select_part inter in
+    chain_left1 union_part union mparser_state
+  in
+  expr >>= fun e -> return (flatten_round_robin_select e)
+
+let of_string (s : string) : (ast, string) Result.t =
+  parse_string
+    ( expr
+      << spaces
+      >>= fun e ->
+      get_pos
+      >>= fun pos ->
+      attempt eof
+      >> return e
+         <|> fail (Printf.sprintf "Expected EOI, pos: %s" (string_of_pos pos)) )
+    s ()
+  |> result_of_mparser_result
---
+++ src/parser_components.ml
@@ -0,0 +1,154 @@
+open MParser
+
+let alpha_string : (string, unit) t = many1_chars letter
+
+let any_string : (string, unit) t = many_satisfy (fun _ -> true)
+
+let take_space : (string, unit) t = many_chars space
+
+let ident_string ~(reserved_words : string list) : (string, unit) t =
+  let reserved_words = List.map String.lowercase_ascii reserved_words in
+  alpha_string
+  >>= fun s ->
+  if List.mem (String.lowercase_ascii s) reserved_words then
+    fail (Printf.sprintf "\"%s\" is a reserved word" s)
+  else return s
+
+let skip_non_num_string ~end_markers =
+  skip_satisfy (function
+      | '0' .. '9' -> false
+      | c -> (
+          match end_markers with
+          | None -> true
+          | Some x -> not (String.contains x c) ))
+
+let nat_zero : (int, unit) t =
+  many1_satisfy (function '0' .. '9' -> true | _ -> false)
+  >>= fun s ->
+  try return (int_of_string s)
+  with _ -> fail (Printf.sprintf "Integer %s is out of range" s)
+
+let float_non_neg : (float, unit) t =
+  many1_satisfy (function '0' .. '9' -> true | _ -> false)
+  >>= fun x ->
+  attempt
+    (char '.' >> many1_satisfy (function '0' .. '9' -> true | _ -> false))
+  <|> return "0"
+  >>= fun y ->
+  let s = x ^ "." ^ y in
+  try return (float_of_string s)
+  with _ -> fail (Printf.sprintf "Float %s is out of range" s)
+
+let comma : (char, unit) t = char ','
+
+let dot : (char, unit) t = char '.'
+
+let hyphen : (char, unit) t = char '-'
+
+let non_square_bracket_string : (string, unit) t =
+  many_satisfy (function '[' | ']' -> false | _ -> true)
+
+let non_parenthesis_string : (string, unit) t =
+  many_satisfy (function '(' | ')' -> false | _ -> true)
+
+let non_space_string : (string, unit) t = many_chars non_space
+
+let sep_by_comma (p : ('a, unit) t) : ('a list, unit) t =
+  sep_by p (attempt (spaces >> comma >> spaces))
+
+let sep_by_comma1 (p : ('a, unit) t) : ('a list, unit) t =
+  sep_by1 p (attempt (spaces >> comma >> spaces))
+
+let option (default : 'a) p : ('a, 'b) t = attempt p <|> return default
+
+let string_of_pos pos =
+  let _index, lnum, cnum = pos in
+  Printf.sprintf "%d:%d" lnum cnum
+
+(* let sep_res_seq ~by ~end_markers (p : ('a, 'b) t) : ('a result Seq.t, 'b) t =
+ *   sep_by (char by)
+ *     ( get_pos
+ *       >>= fun pos ->
+ *       many1_satisfy (fun c -> c <> by && not (String.contains end_markers c))
+ *       >>= fun s -> return (pos, s) )
+ *   |>> fun l ->
+ *     ( l
+ *       |> List.to_seq
+ *       |> Seq.map (fun (pos, s) ->
+ *           match
+ *           parse_string
+ *             ( p
+ *               >>= fun x ->
+ *               attempt eoi >> return x
+ *               <|> ( get_pos
+ *                     >>= fun pos ->
+ *                     any_string
+ *                     >>= fun s -> (fail (Printf.sprintf "Invalid syntax: %s, pos: %s" s (string_of_pos pos) )) )
+ *             )
+ *             s
+ *             ()
+ *           with
+ *           | Success 
+ *     )
+ * 
+ * let sep_res ~by ~end_markers (p : 'a t) : ('a, string) result list t =
+ *   sep_res_seq ~by ~end_markers p >>= fun s -> return (List.of_seq s)
+ * 
+ * let sep_fail_on_first_fail ~by ~end_markers (p : 'a t) : 'a list t =
+ *   sep_res_seq ~by ~end_markers p
+ *   >>= fun s ->
+ *   match Seq_utils.find_first_error_or_return_whole_list s with
+ *   | Ok l -> return l
+ *   | Error s -> fail s *)
+
+(* let chainl1 x op =
+ *   let rec aux a =
+ *     attempt (spaces >> op << spaces)
+ *     >>= (fun f -> x >>= fun b -> aux (f a b))
+ *         <|> return a
+ *   in
+ *   x >>= aux *)
+
+let invalid_syntax ~text ~pos =
+  fail (Printf.sprintf "Invalid syntax: %s, pos: %s" text (string_of_pos pos))
+
+let extraneous_text_check ~end_markers =
+  spaces
+  >> get_pos
+  >>= fun pos ->
+  many_satisfy (fun c -> not (String.contains end_markers c))
+  >>= fun s ->
+  match s with "" -> return () | text -> invalid_syntax ~text ~pos
+
+let result_of_mparser_result (x : 'a result) : ('a, string) Result.t =
+  match x with
+  | Success x -> Ok x
+  | Failed (s, err) -> (
+      match err with
+      | No_error -> Error "Unknown error"
+      | Parse_error (pos, msgs) -> (
+          match
+            List.fold_left
+              (fun res msg ->
+                 match res with
+                 | Some x -> Some x
+                 | None -> (
+                     match msg with
+                     | Unexpected_error s ->
+                       Some
+                         (Printf.sprintf "Unexpected: %s, pos: %s" s
+                            (string_of_pos pos))
+                     | Expected_error s ->
+                       Some
+                         (Printf.sprintf "Expected: %s, pos: %s" s
+                            (string_of_pos pos))
+                     | Message_error s -> Some s
+                     | Compound_error (s, _) -> Some s
+                     | Backtrack_error _ -> res
+                     | Unknown_error -> res ))
+              None msgs
+          with
+          | None ->
+            Error
+              (Printf.sprintf "Unknown error, pos: %s" (string_of_pos pos))
+          | Some s -> Error s ) )
