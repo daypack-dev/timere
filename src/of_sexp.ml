b6a616de6335487d5c95f3f986ed0d393d91d755
Darren Ldl <darrenldldev@gmail.com>
2020-12-02 23:11:32 +1100

    Progress on Of_sexp

--- src/of_sexp.ml
+++ src/of_sexp.ml
@@ -56,3 +56,58 @@ let range_of_sexp ~(f : CCSexp.t -> 'a) (x : CCSexp.t) =
   | `List [ `Atom "range_inc"; x; y] -> `Range_inc (f x, f y)
   | `List [ `Atom "range_exc"; x; y] -> `Range_exc (f x, f y)
   | _ -> raise Invalid_data
+
+let pattern_of_sexp (x : CCSexp.t) =
+  match x with
+  | `Atom _ -> raise Invalid_data
+  | `List l ->
+    match l with
+    | `Atom "pattern" :: l -> (
+        let years, l =
+          match l with
+          | (`List (`Atom "years" :: years)) :: l ->
+            List.map int_of_sexp years, l
+          | _ -> [], l
+        in
+        let months, l =
+          match l with
+          | (`List (`Atom "months" :: months)) :: l ->
+            List.map month_of_sexp months, l
+          | _ -> [], l
+        in
+        let month_days, l =
+          match l with
+          | (`List (`Atom "month_days" :: month_days)) :: l ->
+            List.map int_of_sexp month_days, l
+          | _ -> [], l
+        in
+        let weekdays, l =
+          match l with
+          | (`List (`Atom "weekdays" :: weekdays)) :: l ->
+            List.map weekday_of_sexp weekdays, l
+          | _ -> [], l
+        in
+        let hours, l =
+          match l with
+          | (`List (`Atom "hours" :: hours)) :: l ->
+            List.map int_of_sexp hours, l
+          | _ -> [], l
+        in
+        let minutes, l =
+          match l with
+          | (`List (`Atom "minutes" :: minutes)) :: l ->
+            List.map int_of_sexp minutes, l
+          | _ -> [], l
+        in
+        let seconds, l =
+          match l with
+          | (`List (`Atom "seconds" :: seconds)) :: l ->
+            List.map int_of_sexp seconds, l
+          | _ -> [], l
+        in
+        match l with
+        | [] ->
+          Time.pattern ~years ~months ~month_days ~weekdays ~hours ~minutes ~seconds ()
+        | _ -> raise Invalid_data
+      )
+    | _ -> raise Invalid_data
