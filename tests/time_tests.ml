004f7d8209af4c9f6334d5aec30b89637c22e2ab
Darren Ldl <darrenldldev@gmail.com>
2020-12-03 19:11:24 +1100

    Formatting

--- tests/time_tests.ml
+++ tests/time_tests.ml
@@ -16,22 +16,20 @@ let search_end_exc = Time.Date_time.to_timestamp search_end_exc_dt
 
 module Qc = struct
   let resolver_is_same_as_simple_resolver =
-    QCheck.Test.make ~count:5 ~name:"resolver_is_same_as_simple_resolver"
-      time (fun t ->
-          OSeq.equal ~eq:( = )
-            ( Result.get_ok
-              @@ Resolver.resolve
-                Time.(
-                  inter t (interval_dt_exc search_start_dt search_end_exc_dt)) )
-            (Simple_resolver.resolve ~search_start ~search_end_exc ~tz_offset_s:0
-               t))
+    QCheck.Test.make ~count:5 ~name:"resolver_is_same_as_simple_resolver" time
+      (fun t ->
+         OSeq.equal ~eq:( = )
+           ( Result.get_ok
+             @@ Resolver.resolve
+               Time.(
+                 inter t (interval_dt_exc search_start_dt search_end_exc_dt)) )
+           (Simple_resolver.resolve ~search_start ~search_end_exc ~tz_offset_s:0
+              t))
 
   let to_of_sexp =
     QCheck.Test.make ~count:100_000 ~name:"to_of_sexp" time (fun t ->
         let t' = t |> To_sexp.to_sexp |> Of_sexp.of_sexp |> Result.get_ok in
         Time.equal t t')
 
-  let suite = [
-    resolver_is_same_as_simple_resolver;
-    to_of_sexp ]
+  let suite = [ resolver_is_same_as_simple_resolver; to_of_sexp ]
 end
