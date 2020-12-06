91398db020097d7b2c478f1afa6ffd9adeb920d0
Darren Ldl <darrenldldev@gmail.com>
2020-12-06 15:45:24 +1100

    Added fuzzing code

--- fuzz/resolver_is_same_as_simple_resolver.ml
+++ fuzz/resolver_is_same_as_simple_resolver.ml
@@ -1,4 +1,26 @@
-open Crowbar
 open Fuzz_utils
 
-let () = ()
+let search_start_dt =
+  Result.get_ok
+  @@ Time.Date_time.make ~year:2000 ~month:`Jan ~day:1 ~hour:0 ~minute:0
+    ~second:0 ~tz_offset_s:0
+
+let search_start = Time.Date_time.to_timestamp search_start_dt
+
+let search_end_exc_dt =
+  Result.get_ok
+  @@ Time.Date_time.make ~year:2003 ~month:`Jan ~day:1 ~hour:0 ~minute:0
+    ~second:0 ~tz_offset_s:0
+
+let search_end_exc = Time.Date_time.to_timestamp search_end_exc_dt
+
+let () =
+  Crowbar.add_test ~name:"resolver_is_same_as_simple_resolver" [ time ]
+    (fun t ->
+       Crowbar.check_eq ~eq:(OSeq.equal ~eq:( = ))
+         (Result.get_ok
+          @@ Resolver.resolve
+            Time.(
+              inter [ t; interval_dt_exc search_start_dt search_end_exc_dt ]))
+         (Simple_resolver.resolve ~search_start ~search_end_exc
+            ~search_using_tz_offset_s:0 t))
