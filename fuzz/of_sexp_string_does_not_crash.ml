d2c9fe702067d98031b6fe2c71e643ab545506ae
Darren Ldl <darrenldldev@gmail.com>
2020-12-07 14:34:53 +1100

    Formatting

--- fuzz/of_sexp_string_does_not_crash.ml
+++ fuzz/of_sexp_string_does_not_crash.ml
@@ -1,6 +1,5 @@
 open Fuzz_utils
 
 let () =
-  Crowbar.add_test ~name:"of_sexp_string_does_not_crash" [ Crowbar.bytes ] (fun s ->
-      Of_sexp.of_sexp_string s |> ignore;
-      )
+  Crowbar.add_test ~name:"of_sexp_string_does_not_crash" [ Crowbar.bytes ]
+    (fun s -> Of_sexp.of_sexp_string s |> ignore)
