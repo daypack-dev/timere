6e2e620ece56529b21c2fb1f4f039b59b50c99ef
Darren Ldl <darrenldldev@gmail.com>
2020-12-21 01:25:36 +1100

    Added workaround for max_timestamp

--- src/time.ml
+++ src/time.ml
@@ -1382,7 +1382,8 @@ let cur_timestamp () : int64 = Unix.time () |> Int64.of_float
 
 let min_timestamp = Ptime.min |> Ptime.to_float_s |> Int64.of_float
 
-let max_timestamp = Ptime.max |> Ptime.to_float_s |> Int64.of_float
+let max_timestamp =
+  Ptime.max |> Ptime.to_float_s |> Int64.of_float |> Int64.pred
 
 module Date_time = struct
   exception Invalid_date_time
