8d57bb1de371366c313b74c6e8d6d6d644cc7e91
Darren Ldl <darrenldldev@gmail.com>
2020-12-27 19:19:59 +1100

    Tuning API

--- src/RFC3339.ml
+++ src/RFC3339.ml
@@ -5,11 +5,13 @@ let of_date_time (dt : Time.Date_time.t) : string =
     (match dt.tz_info with
      | `Tz_only _ -> invalid_arg "Date time has no exact offset"
      | `Tz_offset_s_only x | `Tz_and_tz_offset_s (_, x) ->
-       let sign = if x < 0 then '-' else '+' in
-       let offset =
-         Result.get_ok @@ Duration.of_seconds (Int64.of_int (abs x))
-       in
-       Printf.sprintf "%c%02d:%02d" sign offset.hours offset.minutes)
+       if x = 0 then "Z"
+       else
+         let sign = if x < 0 then '-' else '+' in
+         let offset =
+           Result.get_ok @@ Duration.of_seconds (Int64.of_int (abs x))
+         in
+         Printf.sprintf "%c%02d:%02d" sign offset.hours offset.minutes)
 
 let pp_date_time formatter dt = Format.fprintf formatter "%s" (of_date_time dt)
 
