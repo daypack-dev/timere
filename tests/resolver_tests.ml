12d7b75b96a4ffcaf124c0377d0ea334ccfcbcc5
Darren Ldl <darrenldldev@gmail.com>
2020-12-29 23:29:08 +1100

    Adding resolver tests

--- tests/resolver_tests.ml
+++ tests/resolver_tests.ml
@@ -31,7 +31,7 @@ module Qc = struct
               | [] -> false
               | r ->
                 let _xr, yr = List.hd @@ List.rev r in
-                not (List.exists (fun (x2, _y2) -> x2 < x1 && yr <= x2) l2))
+                not (List.exists (fun (x2, _y2) -> yr <= x2 && x2 < x1) l2))
            s)
 
   let find_after_completeness =
@@ -83,7 +83,7 @@ module Qc = struct
                   | [] -> false
                   | [ (xr2, _yr2) ] ->
                     not
-                      (List.exists (fun (x2, _y2) -> x2 < xr2 && yr1 <= x2) l2)
+                      (List.exists (fun (x2, _y2) -> yr1 <= x2 && x2 < xr2) l2)
                   | _ -> false)
               | _ -> false)
            s)
@@ -108,6 +108,60 @@ module Qc = struct
               | (_xr, yr) :: _ -> OSeq.mem ~eq:( = ) (x1, yr) s)
            l1)
 
+  let find_between_exc_empty =
+    QCheck.Test.make ~count:10_000 ~name:"find_between_exc_empty"
+      QCheck.(
+        triple pos_int64 sorted_time_slots_maybe_gaps
+          sorted_time_slots_maybe_gaps)
+      (fun (bound, l1, l2) ->
+         let s1 = List.to_seq l1 in
+         let s2 = List.to_seq l2 in
+         let s = Resolver.find_between_exc bound s1 s2 in
+         (not (OSeq.is_empty s1 || OSeq.is_empty s2)) || OSeq.is_empty s)
+
+  let find_between_exc_soundness =
+    QCheck.Test.make ~count:10_000 ~name:"find_between_exc_soundness"
+      QCheck.(
+        triple pos_int64 sorted_time_slots_maybe_gaps
+          sorted_time_slots_maybe_gaps)
+      (fun (bound, l1, l2) ->
+         let s1 = List.to_seq l1 in
+         let s2 = List.to_seq l2 in
+         let s = Resolver.find_between_exc bound s1 s2 in
+         OSeq.for_all
+           (fun (x1, y1) ->
+              match List.filter (fun (x2, _y2) -> x1 = x2) l1 with
+              | [] -> false
+              | [ (_xr1, yr1) ] -> (
+                  match List.filter (fun (x2, _y2) -> y1 = x2) l2 with
+                  | [] -> false
+                  | [ (xr2, _yr2) ] ->
+                    not
+                      (List.exists (fun (x2, _y2) -> yr1 <= x2 && x2 < xr2) l2)
+                  | _ -> false)
+              | _ -> false)
+           s)
+
+  let find_between_exc_completeness =
+    QCheck.Test.make ~count:10_000 ~name:"find_between_exc_completeness"
+      QCheck.(
+        triple pos_int64 sorted_time_slots_maybe_gaps
+          sorted_time_slots_maybe_gaps)
+      (fun (bound, l1, l2) ->
+         let s1 = List.to_seq l1 in
+         let s2 = List.to_seq l2 in
+         let s = Resolver.find_between_exc bound s1 s2 in
+         List.for_all
+           (fun (x1, y1) ->
+              match
+                List.filter
+                  (fun (x2, _y2) -> y1 <= x2 && Int64.sub x2 y1 <= bound)
+                  l2
+              with
+              | [] -> true
+              | (xr, _yr) :: _ -> OSeq.mem ~eq:( = ) (x1, xr) s)
+           l1)
+
   let suite =
     [
       find_after_empty;
@@ -116,5 +170,8 @@ module Qc = struct
       find_between_inc_empty;
       find_between_inc_soundness;
       find_between_inc_completeness;
+      find_between_exc_empty;
+      find_between_exc_soundness;
+      find_between_exc_completeness;
     ]
 end
