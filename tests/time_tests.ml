ea9e4504aaf6ca0faef562ac03189b48c97095f1
Darren Ldl <darrenldldev@gmail.com>
2020-12-30 16:56:49 +1100

    Adding more time tests

--- tests/time_tests.ml
+++ tests/time_tests.ml
@@ -38,5 +38,59 @@ module Qc = struct
         let t' = t |> To_sexp.to_sexp |> Of_sexp.of_sexp |> Result.get_ok in
         Time.equal t t')
 
-  let suite = [ (* resolver_is_same_as_simple_resolver; *) to_of_sexp ]
+  let union_order_does_not_matter =
+    QCheck.Test.make ~count:10_000 ~name:"union_order_does_not_matter" QCheck.(list time) (fun l1 ->
+        let l2 = List.rev l1 in
+        let l3 =
+          let x =
+            l1 |> List.to_seq |> OSeq.take_nth 2
+            |> List.of_seq
+          in
+          let y =
+            l1 |> List.to_seq |> OSeq.drop 1 |> OSeq.take_nth 2
+            |> List.of_seq
+          in
+          x @ y
+        in
+        let t1 = Time.union l1 in
+        let t2 = Time.union l2 in
+        let t3 = Time.union l3 in
+        let r1 = Result.get_ok @@ Resolver.resolve t1 in
+        let r2 = Result.get_ok @@ Resolver.resolve t2 in
+        let r3 = Result.get_ok @@ Resolver.resolve t3 in
+        OSeq.equal ~eq:( = ) r1 r2
+        &&
+        OSeq.equal ~eq:( = ) r2 r3
+      )
+
+  let inter_order_does_not_matter =
+    QCheck.Test.make ~count:10_000 ~name:"inter_order_does_not_matter" QCheck.(list time) (fun l1 ->
+        let l2 = List.rev l1 in
+        let l3 =
+          let x =
+            l1 |> List.to_seq |> OSeq.take_nth 2
+            |> List.of_seq
+          in
+          let y =
+            l1 |> List.to_seq |> OSeq.drop 1 |> OSeq.take_nth 2
+            |> List.of_seq
+          in
+          x @ y
+        in
+        let t1 = Time.inter l1 in
+        let t2 = Time.inter l2 in
+        let t3 = Time.inter l3 in
+        let r1 = Result.get_ok @@ Resolver.resolve t1 in
+        let r2 = Result.get_ok @@ Resolver.resolve t2 in
+        let r3 = Result.get_ok @@ Resolver.resolve t3 in
+        OSeq.equal ~eq:( = ) r1 r2
+        &&
+        OSeq.equal ~eq:( = ) r2 r3
+      )
+
+  let suite = [ (* resolver_is_same_as_simple_resolver; *)
+    (* to_of_sexp; *)
+    (* union_order_does_not_matter; *)
+    inter_order_does_not_matter;
+  ]
 end
