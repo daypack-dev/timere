25fc5200c46b9538cf82d58488fb12aab82c3c77
Darren Ldl <darrenldldev@gmail.com>
2021-01-06 01:33:48 +1100

    Added tz.none

---
+++ tz-none/timere_tz_data.ml
@@ -0,0 +1,15 @@
+type entry = {
+  is_dst : bool;
+  offset : int;
+}
+
+type table = (int64 * entry) array
+
+type record = {
+  recorded_offsets : int array;
+  table : table;
+}
+
+let lookup_record _ = None
+
+let available_time_zones () = []
