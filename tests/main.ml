06cc8477d1b2e85ae24eec011f4cb9868ae9c29d
Darren Ldl <darrenldldev@gmail.com>
2021-01-07 08:46:26 +1100

    Added Time_zone_tests to test suite

--- tests/main.ml
+++ tests/main.ml
@@ -48,6 +48,7 @@ let () =
       ("Time_intervals_tests.Qc", Time_intervals_tests.Qc.suite);
       ("Time_tests.Qc", Time_tests.Qc.suite);
       ("Resolver_tests.Qc", Resolver_tests.Qc.suite);
+      ("Time_zone_tests.Qc", Time_zone_tests.Qc.suite);
     ]
     |> List.map (fun (name, suite) ->
         (name, List.map QCheck_alcotest.to_alcotest suite))
