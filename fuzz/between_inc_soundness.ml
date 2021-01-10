51494edfbbe5c43ab43add4b618def2db3cced25
Darren Ldl <darrenldldev@gmail.com>
2021-01-10 16:09:25 +1100

    Switched to using internal api for between_inc fuzz

--- fuzz/between_inc_soundness.ml
+++ fuzz/between_inc_soundness.ml
@@ -4,13 +4,13 @@ let () =
   Crowbar.add_test ~name:"between_inc_soundness"
     [ Crowbar.range 100_000; time; time ] (fun bound t1 t2 ->
         let bound = Int64.of_int bound in
-        let s1 = CCResult.get_exn @@ Resolver.resolve t1 in
-        let s2 = CCResult.get_exn @@ Resolver.resolve t2 in
+        let tz = Time_zone.utc in
+        let s1 = Resolver.aux tz t1 in
+        let s2 = Resolver.aux tz t2 in
         let l1 = CCList.of_seq s1 in
         let l2 = CCList.of_seq s2 in
         let s =
-          CCResult.get_exn
-          @@ Resolver.resolve Time.(between_inc (Duration.of_seconds bound) t1 t2)
+          Resolver.(aux_between Inc tz Time.default_search_space bound s1 s2 t1 t2)
         in
         Crowbar.check
           (OSeq.for_all
