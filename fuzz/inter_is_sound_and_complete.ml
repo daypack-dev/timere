40c4b84cdf31e6efdf8e8b343477938e4d67e151
Darren Ldl <darrenldldev@gmail.com>
2021-01-11 11:51:20 +1100

    Adding more fuzzing tests

--- fuzz/inter_is_sound_and_complete.ml
+++ fuzz/inter_is_sound_and_complete.ml
@@ -1,14 +1,14 @@
 open Fuzz_utils
 
 let () =
-  Crowbar.add_test ~name:"union_is_sound_and_complete" [ Crowbar.list1 time ]
+  Crowbar.add_test ~name:"inter_is_sound_and_complete" [ Crowbar.list1 time ]
     (fun l ->
        let tz = Time_zone.utc in
        let s =
-         Resolver.aux_union tz (CCList.to_seq l)
+         Resolver.aux_inter tz (CCList.to_seq l)
        in
        let s' = l |> List.map (Resolver.aux tz) |> CCList.to_seq
-                |> Time.Intervals.Union.union_multi_seq
+                |> Time.Intervals.Inter.inter_multi_seq
        in
        Crowbar.check (
          OSeq.equal ~eq:( = ) s s'
