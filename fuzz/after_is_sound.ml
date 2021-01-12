a0a3c298b21a6d495c8e6f248e3da06891394cd9
Darren Ldl <darrenldldev@gmail.com>
2021-01-12 16:59:16 +1100

    Code refactoring after_is_sound fuzzing test

--- fuzz/after_is_sound.ml
+++ fuzz/after_is_sound.ml
@@ -7,20 +7,20 @@ let () =
        let tz = Time_zone.utc in
        let s1 = Resolver.aux tz t1 in
        let s2 = Resolver.aux tz t2 in
-       let l1 = CCList.of_seq s1 in
        let s =
          Resolver.aux_after tz Time.default_search_space bound s1 s2 t1 t2
        in
        Crowbar.check
          (OSeq.for_all
             (fun (x, _y) ->
-               match
-                 List.filter
+               let r =
+                 OSeq.filter
                    (fun (x1, _y1) -> x1 <= x && Int64.sub x x1 <= bound)
-                   l1
-               with
-               | [] -> false
-               | r ->
-                 let xr, _yr = CCOpt.get_exn @@ Misc_utils.last_element_of_list r in
+                   s1
+               in
+               match r () with
+               | Seq.Nil -> false
+               | _ ->
+                 let xr, _yr = CCOpt.get_exn @@ Seq_utils.last_element_of_seq r in
                  not (OSeq.exists (fun (x2, _y2) -> xr <= x2 && x2 < x) s2))
             s))
