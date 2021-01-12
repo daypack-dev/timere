353a93d8671a4337f7bf1689cc4d8566a101790a
Darren Ldl <darrenldldev@gmail.com>
2021-01-12 13:55:02 +1100

    Updated run.sh

--- fuzz/run.sh
+++ fuzz/run.sh
@@ -27,11 +27,11 @@ mkdir -p "$input_dir"
 
 echo "abcd" > "$input_dir"/dummy
 
-#if [ -d "$output_dir" ]; then
-#  afl-fuzz -t 1000                 -o "$output_dir" "$script_dir"/../_build/default/fuzz/"$name".exe @@
-#else
+if [ -d "$output_dir" ]; then
+  afl-fuzz -t 1000 -i -            -o "$output_dir" "$script_dir"/../_build/default/fuzz/"$name".exe @@
+else
   mkdir -p "$output_dir"
 
   afl-fuzz -t 1000 -i "$input_dir" -o "$output_dir" "$script_dir"/../_build/default/fuzz/"$name".exe @@
-#fi
+fi
 
