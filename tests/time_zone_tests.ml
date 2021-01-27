431caa37212796e662961288886dd349f1bee741
Darren Ldl <darrenldldev@gmail.com>
2021-01-27 21:09:23 +1100

    Added Time_zone to_of_sexp test

--- tests/time_zone_tests.ml
+++ tests/time_zone_tests.ml
@@ -1,6 +1,16 @@
 open Test_utils
 
 module Qc = struct
+  let to_of_sexp =
+    QCheck.Test.make ~count:100_000 ~name:"to_of_sexp" time_zone (fun tz ->
+        let tz' =
+          tz
+          |> Time_zone.Sexp.to_sexp
+          |> Time_zone.Sexp.of_sexp
+          |> CCOpt.get_exn
+        in
+        Time_zone.equal tz tz')
+
   let to_of_json =
     QCheck.Test.make ~count:100_000 ~name:"to_of_json" time_zone (fun tz ->
         let tz' =
@@ -11,5 +21,5 @@ module Qc = struct
         in
         Time_zone.equal tz tz')
 
-  let suite = [ to_of_json ]
+  let suite = [ to_of_sexp; to_of_json ]
 end
