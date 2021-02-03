974ba220a10f698db4b6be78a04239bdf2698983
Darren Ldl <darrenldldev@gmail.com>
2021-02-04 01:37:07 +1100

    Updated debug print

--- debug-parse/main.ml
+++ debug-parse/main.ml
@@ -1,5 +1,9 @@
 let debug_parsing () =
-  let expr = "2020 jan 1 00:00 to 2020 feb 1 00:00" in
+  let expr = "jun 1 00:00 to feb 1 00:00" in
+  print_endline expr;
+  print_newline ();
+  print_endline "gives";
+  print_newline ();
   match Timere_parse.timere expr with
   | Error msg -> print_endline msg
   | Ok timere ->
