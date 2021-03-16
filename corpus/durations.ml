6225ea8bc8b55764f008e3fc75a65eb1eec4d902
Darren Ldl <darrenldldev@gmail.com>
2021-03-16 21:48:59 +1100

    Added corpus for durations

---
+++ corpus/durations.ml
@@ -0,0 +1,16 @@
+let texts = [
+  "24h";
+  "1h20min";
+]
+
+let () =
+  List.iteri (fun i text ->
+      Printf.printf "%d. %S\n" i text;
+      match Timere_parse.duration text with
+      | Ok duration ->
+        Printf.printf "  Ok %s\n\n" (Timere.Duration.to_string duration)
+      | Error msg ->
+        Printf.printf "  Error %s\n" msg;
+        print_endline "  ^^^^^";
+        print_newline ();
+    ) texts
