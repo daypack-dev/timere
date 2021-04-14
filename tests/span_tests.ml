15fec01bf585c0825b33d9c5f7045984ddf1c397
Darren Ldl <darrenldldev@gmail.com>
2021-04-14 16:40:01 +1000

    Increased span test test count

--- tests/span_tests.ml
+++ tests/span_tests.ml
@@ -2,74 +2,74 @@ open Test_utils
 
 module Qc = struct
   let normalize_is_idempotent =
-    QCheck.Test.make ~count:10_000 ~name:"normalize_is_idempotent" timestamp
+    QCheck.Test.make ~count:100_000 ~name:"normalize_is_idempotent" timestamp
       (fun x -> Span.(equal (normalize x) (normalize @@ normalize x)))
 
   let add_sub =
-    QCheck.Test.make ~count:10_000 ~name:"add_sub"
+    QCheck.Test.make ~count:100_000 ~name:"add_sub"
       QCheck.(pair timestamp timestamp)
       (fun (x, y) -> Span.(equal x (x + y - y)))
 
   let sub_add =
-    QCheck.Test.make ~count:10_000 ~name:"sub_add"
+    QCheck.Test.make ~count:100_000 ~name:"sub_add"
       QCheck.(pair timestamp timestamp)
       (fun (x, y) -> Span.(equal x (x - y + y)))
 
   let sub_self =
-    QCheck.Test.make ~count:10_000 ~name:"sub_self" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"sub_self" timestamp (fun x ->
         Span.(equal zero (x - x)))
 
   let double_neg =
-    QCheck.Test.make ~count:10_000 ~name:"double_neg" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"double_neg" timestamp (fun x ->
         Span.(equal x (neg @@ neg x)))
 
   let neg =
-    QCheck.Test.make ~count:10_000 ~name:"neg" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"neg" timestamp (fun x ->
         Span.(equal x (neg (neg x - x) - x)))
 
   let abs =
-    QCheck.Test.make ~count:10_000 ~name:"abs" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"abs" timestamp (fun x ->
         Span.(equal (abs x) (abs (neg x))))
 
   let abs_is_non_neg =
-    QCheck.Test.make ~count:10_000 ~name:"abs_is_non_neg" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"abs_is_non_neg" timestamp (fun x ->
         Span.(abs x >= zero))
 
   let add_commutative =
-    QCheck.Test.make ~count:10_000 ~name:"add_commutative"
+    QCheck.Test.make ~count:100_000 ~name:"add_commutative"
       QCheck.(pair timestamp timestamp)
       (fun (x, y) -> Span.(equal (x + y) (y + x)))
 
   let add_associative =
-    QCheck.Test.make ~count:10_000 ~name:"add_associative"
+    QCheck.Test.make ~count:100_000 ~name:"add_associative"
       QCheck.(triple timestamp timestamp timestamp)
       (fun (x, y, z) -> Span.(equal (x + (y + z)) (x + y + z)))
 
   let add_identity =
-    QCheck.Test.make ~count:10_000 ~name:"add_identity" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"add_identity" timestamp (fun x ->
         Span.(equal (x + zero) (zero + x)))
 
   let neg_then_sub_is_same_as_add_then_neg =
-    QCheck.Test.make ~count:10_000 ~name:"neg_then_sub_is_same_as_add_then_neg"
+    QCheck.Test.make ~count:100_000 ~name:"neg_then_sub_is_same_as_add_then_neg"
       QCheck.(pair timestamp timestamp)
       (fun (x, y) -> Span.(equal (neg x - y) (neg (x + y))))
 
   let neg_distributive1 =
-    QCheck.Test.make ~count:10_000 ~name:"neg_distributive1"
+    QCheck.Test.make ~count:100_000 ~name:"neg_distributive1"
       QCheck.(pair timestamp timestamp)
       (fun (x, y) -> Span.(equal (neg (x + y)) (neg x + neg y)))
 
   let neg_distributive2 =
-    QCheck.Test.make ~count:10_000 ~name:"neg_distributive1"
+    QCheck.Test.make ~count:100_000 ~name:"neg_distributive1"
       QCheck.(pair timestamp timestamp)
       (fun (x, y) -> Span.(equal (neg (x - y)) (neg x - neg y)))
 
   let add_neg_self =
-    QCheck.Test.make ~count:10_000 ~name:"add_neg_self" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"add_neg_self" timestamp (fun x ->
         Span.(equal zero (x + neg x)))
 
   let neg_add_self =
-    QCheck.Test.make ~count:10_000 ~name:"neg_add_self" timestamp (fun x ->
+    QCheck.Test.make ~count:100_000 ~name:"neg_add_self" timestamp (fun x ->
         Span.(equal zero (neg x + x)))
 
   let suite =
