540fd4f5cb7110a77ebf723984fcd32f2f022f71
Darren Ldl <darrenldldev@gmail.com>
2021-04-14 17:25:41 +1000

    Upgraded span test suite

--- tests/span_tests.ml
+++ tests/span_tests.ml
@@ -1,6 +1,16 @@
 open Test_utils
 
 module Qc = struct
+  let normalize_is_lossless =
+    QCheck.Test.make ~count:100_000 ~name:"normalize_is_lossless" QCheck.(pair int64 int)
+      (fun (s, ns) ->
+         let span =
+           Span.normalize { s; ns }
+         in
+         Int64.add (Int64.mul s 1_000_000_000L) (Int64.of_int ns)
+         = Int64.add (Int64.mul Span.(span.s) 1_000_000_000L) (Int64.of_int span.ns)
+      )
+
   let normalize_is_idempotent =
     QCheck.Test.make ~count:100_000 ~name:"normalize_is_idempotent" timestamp
       (fun x -> Span.(equal (normalize x) (normalize @@ normalize x)))
@@ -72,8 +82,16 @@ module Qc = struct
     QCheck.Test.make ~count:100_000 ~name:"neg_add_self" timestamp (fun x ->
         Span.(equal zero (neg x + x)))
 
+  let to_of_float_accurate =
+    QCheck.Test.make ~count:10 ~name:"to_of_float" timestamp (fun x ->
+        let x' = Span.of_float @@ Span.to_float x in
+        x'.s = x.s
+        && ((x'.ns - x.ns) < 1_000)
+      )
+
   let suite =
     [
+      normalize_is_lossless;
       normalize_is_idempotent;
       add_sub;
       sub_add;
@@ -90,5 +108,6 @@ module Qc = struct
       neg_distributive2;
       add_neg_self;
       neg_add_self;
+      to_of_float_accurate;
     ]
 end
