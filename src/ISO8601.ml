0c0e713b0b0ad9a5dce58ffef0665585367693b5
Darren Ldl <darrenldldev@gmail.com>
2021-04-16 18:27:01 +1000

    Formatting

--- src/ISO8601.ml
+++ src/ISO8601.ml
@@ -31,29 +31,20 @@ let to_date_time s : (Time.Date_time'.t, string) result =
            optional (char ':')
            >> two_digit_nat_zero
            >>= fun second ->
-           (choice [char '.'; char ',']) >>
-           num_string >>= (fun s ->
-               let s =
-                 if String.length s > 9 then
-                   String.sub s 0 9
-                 else
-                   s
-               in
-               let len = String.length s in
-               if len = 9 then
-                 return (hour, minute, second, int_of_string s)
-               else
-                 let diff = 9 - len in
-                 let ns =
-                   int_of_float
-                   @@
-                   CCFloat.round
-                     @@
-                     float_of_int (int_of_string s) *. (10. ** float_of_int diff)
-                 in
-                 return (hour, minute, second, ns))
-          )
-        ;
+           choice [ char '.'; char ',' ]
+           >> num_string
+           >>= fun s ->
+           let s = if String.length s > 9 then String.sub s 0 9 else s in
+           let len = String.length s in
+           if len = 9 then return (hour, minute, second, int_of_string s)
+           else
+             let diff = 9 - len in
+             let ns =
+               int_of_float
+               @@ CCFloat.round
+               @@ (float_of_int (int_of_string s) *. (10. ** float_of_int diff))
+             in
+             return (hour, minute, second, ns));
         attempt
           (two_digit_nat_zero
            >>= fun hour ->
@@ -98,8 +89,8 @@ let to_date_time s : (Time.Date_time'.t, string) result =
         offset_p
         >>= fun offset ->
         match
-          Time.Date_time'.make_precise ~year ~month ~day ~hour ~minute ~second ~ns
-            ~tz_offset_s:offset ()
+          Time.Date_time'.make_precise ~year ~month ~day ~hour ~minute ~second
+            ~ns ~tz_offset_s:offset ()
         with
         | None -> fail "Invalid date time"
         | Some x -> return x)
