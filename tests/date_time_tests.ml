0705d57bb4aa50f1ecba81d5beee946dea3fbd1f
Darren Ldl <darrenldldev@gmail.com>
2021-04-16 18:45:02 +1000

    Adding date time tests

---
+++ tests/date_time_tests.ml
@@ -0,0 +1,17 @@
+open Test_utils
+
+module Qc = struct
+  let to_rfc3339_nano_of_iso8601_is_lossless =
+    QCheck.Test.make ~count:100_000
+      ~name:"to_rfc3339_nano_of_iso8601_is_lossless" timestamp (fun timestamp ->
+          let r =
+            CCResult.get_exn @@
+            ISO8601.to_timestamp
+            @@
+            RFC3339.of_timestamp ~precision:9 timestamp
+          in
+          Span.equal r timestamp
+        )
+
+  let suite = [to_rfc3339_nano_of_iso8601_is_lossless]
+end
--- tests/main.ml
+++ tests/main.ml
@@ -47,6 +47,7 @@ let () =
     [
       ("Span_tests.Qc", Span_tests.Qc.suite);
       ("Duration_tests.Qc", Duration_tests.Qc.suite);
+      ("Date_time_tests.Qc", Date_time_tests.Qc.suite);
       ("Time_intervals_tests.Qc", Time_intervals_tests.Qc.suite);
       ("Time_tests.Qc", Time_tests.Qc.suite);
       ("Resolver_tests.Qc", Resolver_tests.Qc.suite);
