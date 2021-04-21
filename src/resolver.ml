79c82716185499d82342908f17fa21b6ba9be85f
Darren Ldl <darrenldldev@gmail.com>
2021-04-22 02:30:35 +1000

    Revert "Fixing resolver"
    This reverts commit 48d98c9ee8ff2a377732397efb200c22bd2fa690.

--- src/resolver.ml
+++ src/resolver.ml
@@ -72,23 +72,23 @@ let calibrate_search_space_for_set (time : t) space : search_space =
       match op with
       | Shift n ->
         let n' = Span.abs n in
-        CCList.filter_map
+        List.map
           Span.(fun (x, y) ->
               (* (min Time.timestamp_max @@ max Time.timestamp_min (x - n),
                *  min Time.timestamp_max @@ max Time.timestamp_min (y - n)
                * ) *)
               if n >= zero then
-                if x - Time.timestamp_min >= n then Some (x - n, y - n)
+                if x - Time.timestamp_min >= n then (x - n, y - n)
                 else if y - Time.timestamp_min >= n then
-                  Some (Time.timestamp_min, y - n)
+                  (Time.timestamp_min, y - n)
                 else
-                  None
+                  (Time.timestamp_min, Time.timestamp_min)
               else
-                if Time.timestamp_max - y >= n' then Some (x + n', y + n')
+                if Time.timestamp_max - y >= n' then (x + n', y + n')
                 else if Time.timestamp_max - x >= n' then
-                  Some (x + n', Time.timestamp_max)
+                  (x + n', Time.timestamp_max)
                 else
-                  None
+                  (Time.timestamp_max, Time.timestamp_max)
             )
           space
       | _ -> space)
