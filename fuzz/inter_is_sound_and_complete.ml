7b817b4273bc9dcc3d2e43dbe8aa3c04aa9f13bc
Darren Ldl <darrenldldev@gmail.com>
2021-04-22 18:57:29 +1000

    Reverting fuzzing test

--- fuzz/inter_is_sound_and_complete.ml
+++ fuzz/inter_is_sound_and_complete.ml
@@ -4,9 +4,7 @@ let () =
   Crowbar.add_test ~name:"inter_is_sound_and_complete"
     [ time_zone; Crowbar.list time ] (fun tz l' ->
         let l = List.map Resolver.t_of_ast l' in
-        let s = Resolver.aux_inter tz (CCList.to_seq l) |> Resolver.normalize
-                |> Time.slice_valid_interval
-        in
+        let s = Resolver.aux_inter tz (CCList.to_seq l) |> Resolver.normalize in
         let s' =
           l
           |> List.map (Resolver.aux tz)
