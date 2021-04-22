44e61697bdaf6fb178e4366e7632d3cc6bc35455
Darren Ldl <darrenldldev@gmail.com>
2021-04-22 16:36:09 +1000

    Fixing resolver tests

--- fuzz/inter_is_sound_and_complete.ml
+++ fuzz/inter_is_sound_and_complete.ml
@@ -4,7 +4,9 @@ let () =
   Crowbar.add_test ~name:"inter_is_sound_and_complete"
     [ time_zone; Crowbar.list time ] (fun tz l' ->
         let l = List.map Resolver.t_of_ast l' in
-        let s = Resolver.aux_inter tz (CCList.to_seq l) |> Resolver.normalize in
+        let s = Resolver.aux_inter tz (CCList.to_seq l) |> Resolver.normalize
+                |> Time.slice_valid_interval
+        in
         let s' =
           l
           |> List.map (Resolver.aux tz)
