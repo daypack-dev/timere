4f878cd23dafa06d7288879c4fdbf8611db50c30
Darren Ldl <darrenldldev@gmail.com>
2021-04-24 17:15:46 +1000

    Updated code

--- src/time_zone_utils.ml
+++ src/time_zone_utils.ml
@@ -3,5 +3,5 @@ let get_local_tz_for_arg () =
   match tz with
   | Some tz -> tz
   | None -> invalid_arg
-              "Could not determine the local timezone. \
+              "get_local_tz_for_arg: Could not determine the local timezone. \
                Please specify ~tz explicitly or use an appropriate timere.tzlocal.* module"
