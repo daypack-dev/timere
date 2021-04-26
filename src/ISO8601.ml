b49b17b23587fb9b81943f514479e5ddea6ebb7a
Darren Ldl <darrenldldev@gmail.com>
2021-04-26 22:38:56 +1000

    Refactoring ISO8601 parser

--- src/ISO8601.ml
+++ src/ISO8601.ml
@@ -3,33 +3,29 @@ open Date_time_components
 let to_date_time s : (Time.Date_time'.t, string) result =
   let open MParser in
   let open Parser_components in
-  let two_digit_nat_zero =
-    pair digit digit
-    >>= fun (c1, c2) -> return (int_of_string (Printf.sprintf "%c%c" c1 c2))
-  in
   let hm_p =
     choice
       [
         attempt
-          (two_digit_nat_zero
+          (max_two_digit_nat_zero
            >>= fun hour ->
            optional (char ':')
-           >> two_digit_nat_zero
+           >> max_two_digit_nat_zero
            >>= fun minute -> return (hour, minute));
-        (two_digit_nat_zero >>= fun hour -> return (hour, 0));
+        (max_two_digit_nat_zero >>= fun hour -> return (hour, 0));
       ]
   in
   let hms_p =
     choice
       [
         attempt
-          (two_digit_nat_zero
+          (max_two_digit_nat_zero
            >>= fun hour ->
            optional (char ':')
-           >> two_digit_nat_zero
+           >> max_two_digit_nat_zero
            >>= fun minute ->
            optional (char ':')
-           >> two_digit_nat_zero
+           >> max_two_digit_nat_zero
            >>= fun second ->
            choice [ char '.'; char ',' ]
            >> num_string
@@ -46,13 +42,13 @@ let to_date_time s : (Time.Date_time'.t, string) result =
              in
              return (hour, minute, second, ns));
         attempt
-          (two_digit_nat_zero
+          (max_two_digit_nat_zero
            >>= fun hour ->
            optional (char ':')
-           >> two_digit_nat_zero
+           >> max_two_digit_nat_zero
            >>= fun minute ->
            optional (char ':')
-           >> two_digit_nat_zero
+           >> max_two_digit_nat_zero
            >>= fun second -> return (hour, minute, second, 0));
         (hm_p |>> fun (hour, minute) -> (hour, minute, 0, 0));
       ]
@@ -72,15 +68,15 @@ let to_date_time s : (Time.Date_time'.t, string) result =
     nat_zero
     >>= fun year ->
     char '-'
-    >> two_digit_nat_zero
+    >> max_two_digit_nat_zero
     >>= fun month ->
     match month_of_human_int month with
     | None -> fail "Invalid month"
     | Some month -> (
         char '-'
-        >> two_digit_nat_zero
+        >> max_two_digit_nat_zero
         >>= fun day ->
-        optional (char 'T')
+        any_char
         >> hms_p
         >>= fun (hour, minute, second, ns) ->
         offset_p
--- src/parser_components.ml
+++ src/parser_components.ml
@@ -37,6 +37,17 @@ let nat_zero_w_original_str : (int * string, unit) t =
   try return (int_of_string s, s)
   with _ -> fail (Printf.sprintf "Integer %s is out of range" s)
 
+let two_digit_nat_zero : (int, unit) t =
+  pair digit digit
+  >>= fun (c1, c2) -> return (int_of_string (Printf.sprintf "%c%c" c1 c2))
+
+let max_two_digit_nat_zero : (int, unit) t =
+  attempt (two_digit_nat_zero)
+  <|> (
+    digit
+    >>= fun c -> return (int_of_string (Printf.sprintf "%c" c))
+  )
+
 let float_non_neg : (float, unit) t =
   many1_satisfy (function '0' .. '9' -> true | _ -> false)
   >>= fun x ->
