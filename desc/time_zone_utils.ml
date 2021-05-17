fbe77bd4edd3967561db8ac0c8d79afc32319a01
Darren Ldl <darrenldldev@gmail.com>
2021-05-17 16:20:09 +1000

    Updated definition of get_local_tz_for_arg

--- desc/time_zone_utils.ml
+++ desc/time_zone_utils.ml
@@ -1,9 +1,6 @@
 let get_local_tz_for_arg () =
-  let tz = Time_zone.local () in
-  match tz with
-  | Some tz -> tz
-  | None ->
-    invalid_arg
-      "get_local_tz_for_arg: Could not determine the local timezone. Please \
-       specify ~tz* explicitly or use an appropriate timere.tzlocal.* \
-       module."
+  CCOpt.get_exn_or
+    "get_local_tz_for_arg: Could not determine the local timezone. Please \
+     specify ~tz* explicitly or use an appropriate timere.tzlocal.* \
+     module."
+    (Time_zone.local ())
