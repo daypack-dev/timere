c39511ad60d4e7d22139cd4d576ac040e268e691
Darren Ldl <darrenldldev@gmail.com>
2021-05-20 19:14:40 +1000

    Fixing timestamp_safe_sub

--- src/resolver.ml
+++ src/resolver.ml
@@ -65,25 +65,23 @@ let get_search_space (time : t) : Time.Interval'.t list =
   | Bounded_intervals { search_space; _ } -> search_space
   | Unchunk (s, _) -> s
 
-let timestamp_safe_sub a b =
+let rec timestamp_safe_sub a b =
   let open Timedesc.Span in
   if b >= zero then
     if a - Timedesc.Timestamp.min_val >= b then a - b
-    else Timedesc.Timestamp.max_val
+    else Timedesc.Timestamp.min_val
   else
     let b' = abs b in
-    if Timedesc.Timestamp.max_val - a >= b' then a + b'
-    else Timedesc.Timestamp.max_val
+    timestamp_safe_add a b'
 
-let timestamp_safe_add a b =
+and timestamp_safe_add a b =
   let open Timedesc.Span in
   if b >= zero then
     if Timedesc.Timestamp.max_val - a >= b then a + b
     else Timedesc.Timestamp.max_val
   else
     let b' = abs b in
-    if a - Timedesc.Timestamp.min_val >= b' then a - b'
-    else Timedesc.Timestamp.min_val
+    timestamp_safe_sub a b'
 
 let calibrate_search_space_for_set (time : t) space : search_space =
   let open Timedesc.Span in
