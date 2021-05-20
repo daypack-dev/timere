7a646147ba58f97d874cca58a1ba61963aee2446
Darren Ldl <darrenldldev@gmail.com>
2021-05-20 20:58:23 +1000

    Added exceptiong raising version of functions for Timere_parse

--- parse/timere_parse.ml
+++ parse/timere_parse.ml
@@ -1,7 +1,21 @@
+exception Error_exn of string
+
 let timere = Parser.parse_timere
+
+let timere_exn x =
+  match timere x with Ok x -> x | Error msg -> raise (Error_exn msg)
 
 let date_time = Parser.parse_date_time
 
+let date_time_exn ?tz x =
+  match date_time ?tz x with Ok x -> x | Error msg -> raise (Error_exn msg)
+
 let hms = Parser.parse_hms
 
+let hms_exn x =
+  match hms x with Ok x -> x | Error msg -> raise (Error_exn msg)
+
 let duration = Parser.parse_duration
+
+let duration_exn x =
+  match duration x with Ok x -> x | Error msg -> raise (Error_exn msg)
--- parse/timere_parse.mli
+++ parse/timere_parse.mli
@@ -1,8 +1,18 @@
+exception Error_exn of string
+
 val timere : string -> (Timere.t, string) result
+
+val timere_exn : string -> Timere.t
 
 val date_time :
   ?tz:Timedesc.Time_zone.t -> string -> (Timedesc.t, string) result
 
+val date_time_exn : ?tz:Timedesc.Time_zone.t -> string -> Timedesc.t
+
 val hms : string -> (Timere.Hms.t, string) result
 
+val hms_exn : string -> Timere.Hms.t
+
 val duration : string -> (Timedesc.Span.t, string) result
+
+val duration_exn : string -> Timedesc.Span.t
