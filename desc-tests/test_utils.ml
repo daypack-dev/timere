0830cba62dcba967954f5a03218afb04a37b30d2
Darren Ldl <darrenldldev@gmail.com>
2021-06-02 17:57:52 +1000

    Added zoneless tests

--- desc-tests/test_utils.ml
+++ desc-tests/test_utils.ml
@@ -289,7 +289,16 @@ let span_testable : (module Alcotest.TESTABLE with type t = Timedesc.Span.t) =
     let equal = Timedesc.Span.equal
   end)
 
-let date_time_testable : (module Alcotest.TESTABLE) =
+let zoneless_testable : (module Alcotest.TESTABLE with type t = Timedesc.Zoneless.zoneless) =
+  (module struct
+    type t = Timedesc.Zoneless.zoneless
+
+    let pp formatter t = Timedesc.pp () formatter (Timedesc.Zoneless.to_zoned_exn ~tz:Timedesc.Time_zone.utc t)
+
+    let equal = Timedesc.Zoneless.equal
+  end)
+
+let date_time_testable : (module Alcotest.TESTABLE with type t = Timedesc.t) =
   (module struct
     type t = Timedesc.t
 
@@ -297,6 +306,25 @@ let date_time_testable : (module Alcotest.TESTABLE) =
 
     let equal = Timedesc.equal
   end)
+
+let maybe_zoneless_testable : (module Alcotest.TESTABLE with type t = [ `Zoned of Timedesc.t | `Zoneless of Timedesc.Zoneless.zoneless ]) =
+  (module struct
+    type t = [ `Zoned of Timedesc.t | `Zoneless of Timedesc.Zoneless.zoneless ]
+
+    let pp formatter (t : t) =
+      match t with
+      | `Zoned t ->
+        Timedesc.pp () formatter t
+      | `Zoneless t ->
+        Timedesc.pp () formatter (Timedesc.Zoneless.to_zoned_exn ~tz:Timedesc.Time_zone.utc t)
+
+    let equal x y =
+      match x, y with
+      | `Zoned x, `Zoned y -> Timedesc.equal x y
+      | `Zoneless x, `Zoneless y -> Timedesc.Zoneless.equal x y
+      | _, _ -> false
+  end)
+
 
 let tz_testable : (module Alcotest.TESTABLE with type t = Timedesc.Time_zone.t)
   =
