a8086fdfe714698885a7bd08ce5f21c34b0898a7
Darren Ldl <darrenldldev@gmail.com>
2022-02-04 22:56:01 +1100

    Added tests of iso8601 printers and parsers for three date systems

--- desc-tests/date_tests.ml
+++ desc-tests/date_tests.ml
@@ -88,6 +88,42 @@ module Qc = struct
         in
         Timedesc.Date.equal d d')
 
+  let ymd_to_iso8601_of_iso8601 =
+    QCheck.Test.make ~count:100_000 ~name:"ymd_to_iso8601_of_iso8601" ymd_date
+      (fun (year, month, day) ->
+        let d = Timedesc.Date.Ymd.make_exn ~year ~month ~day in
+        let d' =
+          d
+          |> Timedesc.Date.Ymd.to_iso8601
+          |> Timedesc.Date.Ymd.of_iso8601
+          |> CCResult.get_exn
+        in
+        Timedesc.Date.equal d d')
+
+  let iso_week_date_to_iso8601_of_iso8601 =
+    QCheck.Test.make ~count:100_000 ~name:"iso_week_date_to_iso8601_of_iso8601"
+      iso_week_date (fun (year, week, weekday) ->
+        let d = Timedesc.Date.ISO_week_date.make_exn ~year ~week ~weekday in
+        let d' =
+          d
+          |> Timedesc.Date.ISO_week_date.to_iso8601
+          |> Timedesc.Date.ISO_week_date.of_iso8601
+          |> CCResult.get_exn
+        in
+        Timedesc.Date.equal d d')
+
+  let iso_ord_to_iso8601_of_iso8601 =
+    QCheck.Test.make ~count:100_000 ~name:"iso_ord_to_iso8601_of_iso8601"
+      iso_ord_date (fun (year, day_of_year) ->
+        let d = Timedesc.Date.ISO_ord.make_exn ~year ~day_of_year in
+        let d' =
+          d
+          |> Timedesc.Date.ISO_ord.to_iso8601
+          |> Timedesc.Date.ISO_ord.of_iso8601
+          |> CCResult.get_exn
+        in
+        Timedesc.Date.equal d d')
+
   let to_of_sexp =
     QCheck.Test.make ~count:100_000 ~name:"to_of_sexp" ymd_date
       (fun (year, month, day) ->
@@ -172,6 +208,9 @@ module Qc = struct
   let suite =
     [
       to_rfc3339_of_iso8601;
+      ymd_to_iso8601_of_iso8601;
+      iso_week_date_to_iso8601_of_iso8601;
+      iso_ord_to_iso8601_of_iso8601;
       to_of_sexp;
       view_is_same_as_original_iso_ord_date;
       view_is_same_as_original_iso_week_date;
