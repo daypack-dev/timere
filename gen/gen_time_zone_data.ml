38842ac7c93ec1d6a8b83cfff7490b055df50633
Darren Ldl <darrenldldev@gmail.com>
2022-06-06 17:00:30 +1000

    Added generated artifact

--- gen/gen_time_zone_data.ml
+++ gen/gen_time_zone_data.ml
@@ -434,12 +434,12 @@ let () =
   in
   let db = Timedesc.Time_zone.Db.of_seq @@ CCList.to_seq time_zones in
 
-  Printf.printf "Generating sexp serialization of tzdb %s\n" sexp_output_file_name;
+  Printf.printf "Generating sexp serialization of tzdb: %s\n" sexp_output_file_name;
   CCIO.with_out ~flags:output_flags sexp_output_file_name (fun oc ->
       Format.fprintf (CCFormat.of_chan oc) "%a@." Sexp.pp
         (Timedesc.Time_zone.Db.Sexp.to_sexp db));
 
-  Printf.printf "Generating compressed serialization of tzdb %s\n" compressed_output_file_name;
+  Printf.printf "Generating compressed serialization of tzdb: %s\n" compressed_output_file_name;
   CCIO.with_out ~flags:output_flags compressed_output_file_name (fun oc ->
       Format.fprintf (CCFormat.of_chan oc) "let s = %S@."
         (Timedesc.Time_zone.Db.Compressed.to_string db));
