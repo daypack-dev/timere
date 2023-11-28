fab708899606047839b17d4fd913880cc51ec5a4
Darren Li <darrenldldev@gmail.com>
2023-11-28 19:09:45 +1100

    Refreshed container dev setup stuff

--- start-container-timere-fuzz.sh
+++ start-container-timere-fuzz.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-podman run -it -v ~/timere:/root/timere --rm localhost/timere-fuzz
+podman run -it \
+  -v ~/timere:/home/opam/timere \
+  --userns keep-id:uid=1000,gid=1000 \
+  --workdir /home/opam/timere \
+  --rm \
+  localhost/timere-fuzz
