b63026ce8b9cc2318fce32415625e0cfad1d11b5
Darren Li <darrenldldev@gmail.com>
2024-01-31 00:03:35 +1100

    Updated start-container-timere.sh to use host's time zone info

--- start-container-timere.sh
+++ start-container-timere.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 podman run -it \
   -v ~/timere:/home/opam/timere \
+  -v /usr/share/zoneinfo:/usr/share/zoneinfo \
   --userns keep-id:uid=1000,gid=1000 \
   --workdir /home/opam/timere \
   --rm \
