9b172c78824bc5fd151cfbd1720785fd250a748e
Darren Li <darrenldldev@gmail.com>
2024-04-11 09:48:38 +1000

    Updated start-container-timere.sh

--- start-container-timere.sh
+++ start-container-timere.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 podman run -it \
   -v ~/timere:/home/opam/timere \
-  -v /usr/share/zoneinfo:/usr/share/zoneinfo \
+  -v $TZDIR:/usr/share/zoneinfo \
   --userns keep-id:uid=1000,gid=1000 \
   --workdir /home/opam/timere \
   --rm \
