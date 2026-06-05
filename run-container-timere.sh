bcae3f1535b648d2483d4edc70591e97a5f4e9d6
Darren <darrenldldev@gmail.com>
2026-06-05 21:23:40 +1000

    Reworking container setup

--- run-container-timere.sh
+++ run-container-timere.sh
@@ -2,7 +2,6 @@
 podman run -it \
   -v ~/timere:/home/opam/timere \
   -v $TZDIR:/usr/share/zoneinfo \
-  --userns keep-id:uid=1000,gid=1000 \
   --workdir /home/opam/timere \
   --rm \
   localhost/timere
